74c762c7136ef5a1b31af861a1cf260710a04fca
Yogesh-dc <yogeshchoudhary1111200@gmail.com>
2026-04-26 20:53:16 +0530

    frontend added

---
+++ Frontend/app.js
@@ -0,0 +1,14 @@
+//express app that serves html file
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log(`Frontend app listening at http://localhost:${port}`);
+});
